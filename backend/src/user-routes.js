5cbcf1de3f92ce0b2eb5a5b513b32612d2dc99b1
Ethan <126639332+Ethan2302@users.noreply.github.com>
2023-04-01 10:28:52 -0500

    Authentication of GET and POST routes

--- backend/src/user-routes.js
+++ backend/src/user-routes.js
@@ -1,218 +1,62 @@
-const User = require("../models/user");
 const express = require("express");
-
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+const User = require("../models/user"); // import the user model
+const { authenticateToken } = require("../middleware/auth"); //import the authentication middleware
+
 const router = express.Router();
 
-// Test user objects
+//Test user objects
 
-// Coach User:
-// ID: 6407fed2f0dc8c869555af28
-// Email: test@test.com
-// Password: 12345
-// Admin User:
-// ID: 6407ff41f0dc8c869555af2a
-// Email: test2@test.com
-// Password: 123456
+//Coach User:
+//ID: 6407fed2f0dc8c869555af28
+//Email: test@test.com
+//Password: 12345
+//Admin User:
+//ID: 6407ff410dc8c869555af2a
+//Email: test2@test.com
+//Password 123456
 
-// ========================================
-
-// Create a new user account
-router.post("/signup", async (req, res) => {
-  const { email, password, accessLevel } = req.body;
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = new User({
-    email,
-    passwordHash: hashedPassword,
-    accessLevel: accessLevel,
-  });
+// User login route
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
   try {
-    await user.save();
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Check if the password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Create and sign a JSON Web Token (JWT)
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h", // Token expires in 1 hour
+    });
+
+    // Return the token to the client
+    res.status(200).json({ token });
   } catch (error) {
-    console.log("Error creating user: ", error);
-    return res.status(500).send({
-      message: "Error creating user",
-    });
-  }
-
-  res.status(201).send({
-    message: "User created",
-  });
-});
-
-// Log in the user
-router.post("/login", async (req, res) => {
-  const user = await user.findOne({ email: req.body.e, ail });
-
-  if (!user || !user.comparePassword(req.body.password))
-    return res.status(400).send("Incorrect Login Information");
-
-  const token = jwt.sign(
-    {
-      _id: user._id,
-    },
-    "CHANGEME!"
-  );
-
-  res.send({ token });
-
-  router.get("/profile", [jwtMiddleware], async (req, res) => {
-    const user = await User.findOne({ _id: req.user._id });
-    res.send(user);
-  });
-
-  router.post(
-    "/sign-up",
-    [...signUpValidators, handleValidationErrors],
-    async (req, res) => {
-      const userExists = await User.findOne({ email: req.body.email });
-
-      if (userExists) return res.status(400).send("E-mail already exists");
-      if ((req.body.password !== req.body.email, req.body.passwordConfirm))
-        return res.status(400).send("Passwords do not match");
-
-      const user = await User.signUp(req.body.email, req.body.password);
-      res.status(201).send(user.sanatize());
-    }
-  );
-
-  router.post(
-    "login",
-    [...loginValidatorsm, handleValidationErrors],
-    async (req, res) => {
-      const user = await User.findOne({ email: req.body.email });
-
-      if (!user || !user.comparePassword(req.body.password))
-        return res.status(400).send("Invalid Login Information");
-
-      const token = jwt.sign(
-        {
-          _id: isSecureContext._id,
-        },
-        "CHANGEME!"
-      );
-
-      res, send({ token });
-    }
-  );
-
-  router.get("/profile", [jwtMiddleware], async (req, res) => {
-    const user = await User.findOne({ _id: req.user._id });
-
-    res.send(user);
-  });
-
-  // --- YOUR CODE GOES UNDER THIS LINE ---
-});
-
-// Log out the user
-router.get("/logout", function (req, res, next) {
-  // remove the req.user property and clear the login session
-  req.logout();
-
-  // destroy session data
-  req.session = null;
-
-  // redirect to homepage
-  res.redirect("/");
-});
-
-router.get("/logout", middleware.isLogedIn, (req, res) => {
-  User.findById(req.user._id).then((rUser) => {
-    rUser.online = false;
-    rUser.save();
-  });
-  req.logout();
-  res.redirect("/");
-});
-
-//Destroys the session to logout the user
-app.get("/logout", function (req, res) {
-  req.session.destroy(() => {
-    req.logout();
-    res.redirect("/"); //Inside a callback… bulletproof!
-  });
-});
-
-app.get("/logout", function (req, res) {
-  //clear the remember me cookie when logging out
-  res.clearCookie("remember_me");
-  req.logout();
-  res.redirect("/");
-});
-
-router.get("/signout", function (req, res) {
-  req.logout();
-  if (!req.session) {
-    req.session.destroy(function (err) {
-      res.redirect("login");
-    });
-  } else {
-    res.redirect("/login");
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
-router.get("logout", isLoggedIn, (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.redirect("/");
+// User logout route
+router.post("/logout", (req, res) => {
+  // Invalidate the user's token on the server side
+  // This can be done in various ways, such as storing the invalidated token
+  // in a database, or using a middleware that checks if the token is still valid
+  // before allowing access to protected routes.
+
+  // For this example, we will simply send a response to the client indicating that
+  // the user has been logged out.
+  res.status(200).json({ message: "User logged out successfully" });
 });
-
-router.get("/logout", function (req, res) {
-  req.logout();
-  res.status(200).json({
-    status: "Bye!",
-  });
-});
-
-//logout logic and route
-router.get("/logout", function (req, res) {
-  req.logout();
-  req.flash("success", "Successfully logged out!");
-  res.redirect("/campgrounds");
-});
-
-logoutRouter.get("/", (req, res) => {
-  req.logout();
-  return res.redirect(HOME);
-});
-
-router.get("/logout", (req, res) => {
-  req.logout();
-  req.session = null;
-  res.redirect(loggedOutHome);
-});
-
-//logout
-app.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
-
-//Logout Route
-
-app.get("/logout", function (req, res) {
-  req.logout();
-  res.redirect("/");
-});
-
-router.get("/logout", isLoggedIn, (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.redirect("/");
-});
-
-router.get("/logout", function (req, res) {
-  req.logout();
-  req.flash("success", "You are logged out of your account");
-  res.redirect("/");
-});
-
-// --- YOUR CODE GOES UNDER THIS LINE ---
 
 module.exports = router;
---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  const prefixedToken = `Bearer ${token}`;
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = {
+  authenticateToken,
+};
